802eca5251f4295ab8e2a6a04c1101a5af4522e7
helena <kotoulova.helena@gmail.com>
2021-11-11 21:30:34 +0100

    pokracovani

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,14 +1,27 @@
-import Card from "../ui/Card";
 import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-function NewMeetUpForm() {
+function NewMeetUpForm(props) {
   const titleInputref = useRef();
+  const imageInputref = useRef();
+  const addressInputref = useRef();
+  const descriptionInputref = useRef();
 
   function submitHandler(event) {
     event.preventDefault(); // bude preventovat to co by browser udelal defaultne
     const enteredTitle = titleInputref.current.value;
+    const enteredImage = imageInputref.current.value;
+    const enteredAddress = addressInputref.current.value;
+    const enteredDescription = descriptionInputref.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+    props.onAddMeetup(meetupData);
   }
 
   return (
@@ -20,15 +33,15 @@ function NewMeetUpForm() {
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputref} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputref} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="description">Meetup Image</label>
-          <textarea required id="description" rows="5"></textarea>
+          <label htmlFor="description">Description</label>
+          <textarea required id="description" rows="5" ref={descriptionInputref}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,23 @@
 import NewMeetUpForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetUpsPage() {
+  function addMeetupHandler(meetupData) {
+    fetch('https://druha-appka-2-default-rtdb.firebaseio.com/meetups.json',
+      // standard js function, that allows us to send http request
+      // my tam dame tu url nasi realtime databaze na firebase. tam se budou odesilat meetupdata.
+      {
+        method: 'POST', // chceme posilat (post), ne odebirat (get - default)
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+  }
   return (
     <section>
       <h1>Added meetups</h1>
-      <NewMeetUpForm />
+      <NewMeetUpForm onAddMeetup={addMeetupHandler}/>
     </section>
   );
 }
